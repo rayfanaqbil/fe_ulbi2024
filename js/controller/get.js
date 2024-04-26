ca0351d762417fbaf5ecbb1bbf4f481474cdaf1b
rayfanaqbil <rayfana09@gmail.com>
2024-04-26 22:38:11 +0700

    update new

--- js/controller/get.js
+++ js/controller/get.js
@@ -4,15 +4,19 @@ import { isiTabel } from "../temp/table.js";
 export function isiTablePresensi(results) {
     results.forEach(isiRow);
 }
-function isiRow(value) {
-    let content =
-        isiTabel.replace("#NAMA#", value.biodata.nama)
+function isiRow(value){
+    let content = 
+    isiTabel.replace("#NAMA#", value.biodata.nama)
             .replace("#NOHP#", value.biodata.phone_number)
             .replace("#JABATAN#", value.biodata.jabatan)
             .replace("#LOKASI#", value.location)
             .replace("#STATUS#", value.checkin)
             .replace("#HARIKERJA#", value.biodata.hari_kerja)
+            .replace("#JAMKERJA#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].durasi + " Jam" : "Jam kerja tidak tersedia")
+            .replace("#JAMMASUK#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].jam_masuk + " Jam" : "Jam masuk tidak tersedia")
+            .replace("#JAMKELUAR#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].jam_keluar + " Jam" : "Jam keluar tidak tersedia")
             .replace("#WARNA#", getRandomColor())
             .replace(/#WARNALOGO#/g, getRandomColorName());
-    addInner("isiTabel", content);
+            
+        addInner("isiTabel", content);
 }
