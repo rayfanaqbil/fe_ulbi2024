ba27f4313325015a399ebb72d47ddcbcaec776ea
rayfanaqbil <rayfana09@gmail.com>
2024-04-26 15:08:51 +0700

    fetch js update

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,11 @@
-import { get } from "https://bukulapak.github.io/api/process.js"; 
+import { get } from "https://bukulapak.github.io/api/process.js";
+import { setInner } from "https://bukulapak.github.io/element/process.js";
 let urlAPI = "https://ws-rayfan2024-7c90fe3029b2.herokuapp.com/presensi";
 get(urlAPI,isiTablePresensi);
 function isiTablePresensi(results){
     console.log(results);
+    results.forEach(isiRow);
+}
+function isiRow(value){
+    console.log(value)
 }
