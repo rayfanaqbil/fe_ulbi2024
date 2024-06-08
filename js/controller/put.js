435460859772f31001bc05a5118a1d4dbb4240dc
rayfanaqbil <rayfana09@gmail.com>
2024-06-08 16:01:27 +0700

    Bisa edit

--- js/controller/put.js
+++ js/controller/put.js
@@ -1,6 +1,6 @@
 import { putData } from "https://bukulapak.github.io/api/process.js";
 import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
-import { urlPUT, AmbilResponse} from "../config/url_post.js";
+import { urlPUT, AmbilResponse} from "../config/url_put.js";
 
 
 function pushData(){
