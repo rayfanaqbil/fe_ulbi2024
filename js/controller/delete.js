6e5a8e4b66b1e9fea40f632ce0de419a9f3b5a82
rayfanaqbil <rayfana09@gmail.com>
2024-06-08 16:09:08 +0700

    Delete

---
+++ js/controller/delete.js
@@ -0,0 +1,17 @@
+function deleteData(IDHAPUS) {
+    var presensiId = IDHAPUS;
+    var target_url = "https://ws-rayfan2024-7c90fe3029b2.herokuapp.com/delete/" + presensiId;
+
+    var requestOptions = {
+        method: 'DELETE',
+        redirect: 'follow'
+    };
+
+    fetch(target_url, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            alert(result.message);
+            location.reload();
+        })
+        .catch(error => console.log('Error:', error));
+}
